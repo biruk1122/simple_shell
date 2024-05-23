39f891b297997a7adf20bdcefd9eda24ff2c4f8f
biruk1122 <birukfikadu29@gmail.com>
2024-05-23 08:11:40 -0700

    updated

--- TEST_SUITE/2.Simple_shell_0.2/echo_1_arg.bash
+++ TEST_SUITE/2.Simple_shell_0.2/echo_1_arg.bash
@@ -1,5 +1,3 @@
-#!/bin/bash
-
 ################################################################################
 # Description for the intranet check (one line, support Markdown syntax)
 # Echo the string `Holberton`
